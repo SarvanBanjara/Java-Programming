be8ab270f24004101c44b46b0c7c93dd8851946e
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-08-29 12:06:33 +0530

    Question 18 and 19 done

--- Patterns/Question_18.java
+++ Patterns/Question_18.java
@@ -5,3 +5,23 @@ package Patterns;
 // 1 2 3
 // 1 2 3 4
 // 1 2 3 4 5
+
+import java.util.Scanner;
+
+public class Question_18 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+        
+        
+        sc.close();
+    }
+}
