cf2413365271a9752fef2084fdeca1ea0778783c
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-08-29 11:31:44 +0530

    Question 10 done

--- Patterns/Question_10.java
+++ Patterns/Question_10.java
@@ -5,3 +5,26 @@ package Patterns;
 //     * * * * *
 //       * * *
 //         *
+
+import java.util.Scanner;
+
+public class Question_10 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n=sc.nextInt();
+        for(int row=n;row>=1;row--){
+            for(int col=1;col<=n-row;col++){
+                System.out.print(" ");
+            }
+            for(int col =1;col<=(row*2)-1;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        
+        sc.close();
+    }
+}
