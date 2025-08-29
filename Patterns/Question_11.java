2e5028fb2b0fa7fd66444f54610d22129cd8b834
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-08-29 11:51:23 +0530

    Question 11 done

--- Patterns/Question_11.java
+++ Patterns/Question_11.java
@@ -5,3 +5,37 @@ package Patterns;
 //     *       *
 //   *           *
 // * * * * * * * * *
+
+import java.util.Scanner;
+
+public class Question_11 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n =sc.nextInt();
+        for(int row=1;row<=n;row++){
+            //printing spaces 
+
+            for(int col=1;col<=n-row;col++){
+                System.out.print("  ");
+            }
+
+            // printing stars
+
+            for(int col=1;col<=(row*2)-1;col++){
+                if(row==1||row==n||col==1||col==(row*2)-1){
+                    System.out.print("* ");
+                }
+                else{System.out.print("  ");}
+            }
+
+            System.out.println();
+
+
+        }
+        
+        
+        sc.close();
+    }
+}
