1b3c6670ed781ffbf813c70c3122d4b0515c6de6
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-08-30 17:21:57 +0530

    Question 28 done

--- Patterns/Question_28.java
+++ Patterns/Question_28.java
@@ -5,3 +5,29 @@ package Patterns;
 //   543
 //    54
 //     5
+
+import java.util.Scanner;
+
+public class Question_28 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+
+            //printing spaces
+            for(int col=1;col<=row;col++){
+                System.out.print(" ");
+            }
+            //printing alphabet
+            for(int col=5;col>=row;col--){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+        
+        
+        sc.close();
+    }
+}
