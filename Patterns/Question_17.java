e08641366909b790b42f1515198f8ece3c4b4f35
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-08-31 21:38:58 +0530

    Question 17 done

--- Patterns/Question_17.java
+++ Patterns/Question_17.java
@@ -1,5 +1,60 @@
 package Patterns;
 
-//     *       *
-//   *   *   *   *
-// *       *       *
+//    *       *
+//  *   *   *   *
+//*       *       *
+
+import java.util.Scanner;
+
+public class Question_17 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length : ");
+        int n = sc.nextInt();
+        for (int row = 1; row <= 3; row++) {
+           
+
+            if (row == 1) {
+                for(int col=1;col<=n;col++){
+                    if(col==1){
+                        System.out.print("    *");
+                    }
+                    else{System.out.print("       *");}
+                }
+                
+
+            }
+
+            else if (row == 2) {
+                for(int col=1;col<=n;col++){
+                    if(col==1){
+                        System.out.print("  *   *");
+                    }
+                    else{System.out.print("   *   *");}
+                }
+                
+
+            }
+
+            else {
+                for(int col=1;col<=n;col++){
+                    if(col==1){
+                        System.out.print("*       *");
+                    }
+                    else{
+                        System.out.print("       *");
+
+                    }
+                }
+            
+
+            }
+
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
