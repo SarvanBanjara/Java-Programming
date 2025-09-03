dced072d4dbdd0321d0d2484b28f9506ef717f97
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-03 14:20:02 +0530

    Question 34 done

--- Patterns/Question_34.java
+++ Patterns/Question_34.java
@@ -5,3 +5,25 @@ package Patterns;
 // 3  6  9 12 15
 // 4  8 12 16 20
 // 5 10 15 20 25
+
+import java.util.Scanner;
+
+public class Question_34 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the height : ");
+            int n=sc.nextInt();
+            for(int row=1;row<=n;row++){
+                int input=row;
+                for(int col=1;col<=n;col++){
+                    System.out.printf("%-3d",input);
+                    input+=row;
+                }
+                System.out.println();
+            }
+        
+        
+        sc.close();
+    }
+}
