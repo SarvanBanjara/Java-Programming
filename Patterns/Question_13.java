a7fce8a13a58f72e3dae0bcb54fbc7ebba87f03e
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-03 13:12:54 +0530

    Question 13 done

--- Patterns/Question_13.java
+++ Patterns/Question_13.java
@@ -1,6 +1,6 @@
 package Patterns;
 
-// * * * * *
+// * * * * *    
 //  * * * *
 //   * * *
 //    * *
@@ -14,23 +14,41 @@ import java.util.Scanner;
 
 public class Question_13 {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the height : ");
-        int n =sc.nextInt();
-        if(n%2==0){
+        int n = sc.nextInt(), mid = (n + 1) / 2;
+        if (n % 2 == 0) {
             n--;
         }
-        for(int row=1;row<=n;row++){
-            //printing 
-            
-            for(int col=1;col<=n;col++){
-                if(col==row  || col == n+1-row){System.out.print("*");}
-                else{System.out.print(" ");}
+        for (int row = 1; row <= mid; row++) {
+
+            // printing spaces
+
+            for (int col = 1; col <= row; col++) {
+                System.out.print(" ");
+            }
+            // printing stars
+            for (int col = 1; col <= mid - row + 1; col++) {
+                System.out.print("* ");
             }
             System.out.println();
-        }        
-        
+        }
+
+        for (int row = 2; row <= mid; row++) {
+
+            // printing spaces
+
+            for (int col = 1; col <= mid - row + 1; col++) {
+                System.out.print(" ");
+            }
+            // printing stars
+            for (int col = 1; col <=row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
         sc.close();
     }
 }
