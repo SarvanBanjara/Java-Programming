0e46b6f70ec262a6c97b8640077fd4ac36ddc381
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-03 13:51:01 +0530

    Question 36 done

--- Patterns/Question_36.java
+++ Patterns/Question_36.java
@@ -1,6 +1,28 @@
 package Patterns;
+
 // 1 2 3 4 5
 // 2 3 4 5 1
 // 3 4 5 1 2
 // 4 5 1 2 3
 // 5 1 2 3 4
+
+import java.util.Scanner;
+
+public class Question_36 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n = sc.nextInt();
+        for (int row = 1; row <= n; row++) {
+            int input = row;
+            for (int col = 1; col <= n; col++) {
+                System.out.print(((input++ - 1) % n) + 1);
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
