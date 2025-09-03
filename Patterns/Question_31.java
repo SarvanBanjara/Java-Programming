7556c9987f2d20b20213234975f77cfb1d92eec1
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-03 14:26:38 +0530

    Question 31 done

--- Patterns/Question_31.java
+++ Patterns/Question_31.java
@@ -4,3 +4,31 @@ package Patterns;
 //       1   1
 //     1   2   1
 //   1   3   3   1
+
+import java.util.Scanner;
+
+public class Question_31 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height : ");
+        int n =sc.nextInt();
+        int input=1;
+        for(int row =1;row<=n;row++){
+            //printing spaces 
+            String inputstring=String.valueOf(input);
+            for(int col=1;col<=n-row;col++){
+                System.out.print("  ");
+            }
+            //printing pattern
+            for(int col=1;col<=row;col++){
+                System.out.print(inputstring.charAt(col-1)+"   ");
+            }
+            input*=11;
+            System.out.println();
+        }
+        
+        
+        sc.close();
+    }
+}
