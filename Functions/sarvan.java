8775c0af8784b2da2b7b2318e7e08c1959507159
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-04 17:50:31 +0530

    Practiced function eg

---
+++ Functions/sarvan.java
@@ -0,0 +1,21 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class sarvan {
+
+    public static int add(int a, int b) {
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the two no : ");
+        int a =sc.nextInt(),b=sc.nextInt();
+        int result =add(a,b); 
+        System.out.printf("The sum of %d and %d is %d",a,b,result);
+
+    }
+
+}
