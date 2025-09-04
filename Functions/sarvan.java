a29eb207048d77605812a02cdacbf0f437140457
Sarvan Banjara <sarvanbanjara2004@gmail.com>
2025-09-04 17:51:14 +0530

    scanner closed

--- Functions/sarvan.java
+++ Functions/sarvan.java
@@ -15,6 +15,7 @@ public class sarvan {
         int a =sc.nextInt(),b=sc.nextInt();
         int result =add(a,b); 
         System.out.printf("The sum of %d and %d is %d",a,b,result);
+        sc.close();
 
     }
 
